74cae5c6763870352e8144cff3393a1569d2a4de
amank037 <138704088+amank037@users.noreply.github.com>
2024-03-17 16:09:41 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -13,3 +13,15 @@ navList.forEach((el,index) => {
     })    
 });    
     
+
+const projectInput = document.querySelectorAll('.project-input');  
+projectInput.forEach(function(radio) {
+    radio.addEventListener('click', function () {
+        if (this.classList.contains('checked')) {
+          this.checked = false;
+          this.classList.remove('checked');
+        } else {
+          this.classList.add('checked');
+        }
+    });
+});
